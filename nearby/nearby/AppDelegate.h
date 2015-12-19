5da94b1da735f97e228e054aa9d2c87323e34a2a
hybridtheorylink <hswlink@gmail.com>
2015-12-19 20:15:02 +0800

    init

---
+++ nearby/nearby/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nearby
+//
+//  Created by Will on 12/19/15.
+//  Copyright © 2015 huawill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nearby/nearby/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nearby
+//
+//  Created by Will on 12/19/15.
+//  Copyright © 2015 huawill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
